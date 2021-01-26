8bbf7ba03786c9d19809f5a4d5ff27f7861f8bf7
Ychanho <77283665+Ychanho@users.noreply.github.com>
2021-01-26 20:37:15 +0900

    데이터 타입 list

---
+++ data_type_list.py
@@ -0,0 +1,60 @@
+# a = [1,2,3,4,5] #정수형
+# print(a)
+# print(len(a))
+#
+# b = ["a","b","c","d"] #정수형
+# print(b)
+# print(len(b))
+#
+# c = [1, "a", 2.5] # 모든 타입 가능
+# print(c)
+# print(len(c))
+#
+a = [1,2,3,4,5]
+b = ["a","b","c","d"]
+print(a[0])
+print(a[1])
+print(a[-1])
+# print(a[0:2])
+# print(a[:2]) # 윗 줄과 동일한 기능
+# print(a[2:])
+# print(a+b)
+# print(a*3)
+
+# d = [1,2,[3,4,5]]
+# print(d)
+# print(len(d))
+# print(d[2][2]) #d의 2번째 에서 2번째의 인덱스가 뭐니( 순서는 0부터 세는것)
+# print(d[2][1:])
+
+# a = [1,2,3,4,5]
+# a[2] = 6 #2번째가 0부터 세서 2번째
+# print(a)
+# print(a.index(4))
+# del(a[2])
+# print(a)
+# del(a[2:])
+# print(a)
+# a.append(999) #추가
+# print(a)
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+#
+##################3333
+# a= [2,1,5,4,3]
+# b =sorted(a) #revers =False가 생략돼 있고 기본은 오름차순
+# b =sorted(a, reverse =True) #내림차순
+# a.sort() #오름차순
+# print(a)
+# a.reverse() #내림차순
+# print(a)
+# a.insert(1,6)
+# print(a)
+# a.remove(6)
+# print(a)
+# print(a.pop()) #맨 마지막 데이터만 뽑아옴
+# print(a.pop())
+# print(a)
+# a.extend([1,0])
+# print(a)
