a8287d18564369a8f637dd15dece2d47b38aaf99
Ychanho <77283665+Ychanho@users.noreply.github.com>
2021-01-27 20:05:42 +0900

    example 숫자,문자열

---
+++ ex01_01,02.py
@@ -0,0 +1,35 @@
+#숫자 예제
+#평균값 구하기
+언어=90
+영어=60
+수학=81
+average = (언어+영어+수학)/3
+print(average)
+
+#문자열 예제
+#1
+print('Mary\'s cosmetics')
+
+#2
+Q2 = "dk2jd923i1jdk2jd93jfd92"
+print(len(Q2))
+
+#3
+t1= 'python'
+t2= 'java'
+print((t1+' '+ t2 +' ' )*3)
+
+#4
+id = "800910-1157963"
+print(id[7])  #()가 아니라 []임
+
+#5 @@ 2212 출력
+l_p = "24가 2210" #license_plate
+
+print(l_p[-4:-1])
+
+
+#6 @@@
+url = "portal.ac.kr"
+url_list = url.split(".")
+print(url_list[2])  #순서는 0번째부터!
