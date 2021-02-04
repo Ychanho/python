b271546889489551f0e918f527e88aa69c91ee1c
Ychanho <77283665+Ychanho@users.noreply.github.com>
2021-02-04 21:42:40 +0900

    Add files via upload

---
+++ dadt_type_set.py
@@ -0,0 +1,10 @@
+#
+# s1 = set("Hello")
+# print(s1)
+#
+s2 = set([1,1,2,2,3,4,5])
+print(s2)
+#
+corpus = ['show', 'me', 'the', 'the', 'a', 'a', 'a', 'money', 'money']
+print(set(corpus))
+print(len(set(corpus)))
